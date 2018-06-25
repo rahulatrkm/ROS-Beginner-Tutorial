b1958fc1d17e563375363765c806c8d78375e3d7
Rahul <rahulatrkm@gmail.com>
2018-06-26 01:17:54 +0530

    completed Creating a ROS msg and srv

--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -7,9 +7,4 @@
 # new environment variables
 
 # modified environment variables
-export CMAKE_PREFIX_PATH="/home/rahul/a/devel:$CMAKE_PREFIX_PATH"
-export LD_LIBRARY_PATH="/home/rahul/a/devel/lib:$LD_LIBRARY_PATH"
-export PKG_CONFIG_PATH="/home/rahul/a/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PWD="/home/rahul/a/build"
-export ROSLISP_PACKAGE_DIRECTORIES="/home/rahul/a/devel/share/common-lisp"
-export ROS_PACKAGE_PATH="/home/rahul/a/src:$ROS_PACKAGE_PATH"
